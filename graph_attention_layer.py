f067a5d729981d991dc76331e8619d60ac8b0dee
V-assim <wassim.robot@gmail.com>
2019-11-28 15:30:39 +0100

    AttentionLayer

--- graph_attention_layer.py
+++ graph_attention_layer.py
@@ -1,12 +1,58 @@
+import numpy as np
 import torch
+import torch.nn.functional as F
 from torch import nn
+import math
+
 
 class AttentionMechanism(nn.Module):
     """docstring for AttentionMechanism."""
 
-    def __init__(self, arg):
+    def __init__(self):
         super(AttentionMechanism, self).__init__()
-        self.arg = arg
+
+
+class AttentionMechanismVaswani(AttentionMechanism):
+
+    def __init__(self,
+                 n_heads,
+                 input_dim,
+                 key_dim,
+                 value_dim):
+        super(AttentionMechanismVaswani, self).__init__()
+
+        self.n_heads = n_heads
+        self.input_dim = input_dim
+        self.key_dim = key_dim
+        self.value_dim = value_dim
+
+        self.compat_factor = 1 / math.sqrt(key_dim)  # faster than **(-0.5)
+
+        self.W_Q = nn.Parameter(torch.tensor((n_heads, input_dim, key_dim)))
+        self.W_K = nn.Parameter(torch.tensor((n_heads, input_dim, key_dim)))
+        self.W_V = nn.Parameter(torch.tensor((n_heads, input_dim, value_dim)))
+
+        # Initialization
+        nn.init.xavier_uniform_(self.W_Q)
+        nn.init.xavier_uniform_(self.W_K)
+        nn.init.xavier_uniform_(self.W_V)
+
+
+    def forward(self, h, mask=None):
+        """
+        h (batch_size, graph_size, input_dim)
+        """
+        
+        q = torch.matmul(h, W_Q)
+        k = torch.matmul(h, W_K)
+        v = torch.matmul(h, W_V)
+
+        # Transform anti-adjacency matrix into 
+        mask = np.clip((0.5 - mask)*np.inf, -np.inf, 1))
+        
+        compatibility = self.compat_factor * torch.matmul(q, k.transpose(2, 3))
+        compatibility = compatibility*mask
+        
 
 
 
