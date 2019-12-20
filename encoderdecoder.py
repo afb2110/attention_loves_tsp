861d85774bf691d06fbfdf7d325a12585b5c8a34
afb2110 <pianot.!2110>
2019-12-20 16:46:53 +0100

    By afb - Decoder finished

--- encoderdecoder.py
+++ encoderdecoder.py
@@ -4,14 +4,17 @@ import os
 import json
 import pprint as pp
 
+import math
 import torch
 from torch import nn, optim
+import torch.nn.functional as F
+import numpy as np
 
 from options import get_options
 from baselines import NoBaseline
 from tsp import TSP as problem
 from train import train_epoch, validate
-from graph_attention_layer import AttentionMechanism, AttentionMechanismVaswani, AttentionMechanismVelickovic
+from graph_attention_layer import AttentionLayer
 
 from utils import log_values, maybe_cuda_model
 
@@ -49,8 +52,10 @@ class Encoder(nn.Module):
 
     def forward(self, x, mask=None):
 
+        assert mask is None, "TODO mask not yet supported!"
+
         # Creating the first embedding h from the data x
-        h = self.first_embed(x)
+        h = self.first_embed(x)  # TOCHECK We may need to change the dimention here
 
         # Going through the N (MHA+FF) layers. We have the final node embeddings.
         h = self.layers(h)
@@ -113,24 +118,127 @@ if __name__ == "__main__":
             )
 
 class Decoder(nn.Module):
-    def __init__(self, embedded_graph, initial_node, final_node,
+    def __init__(self,
             n_heads,
             embed_dim,
-            n_layers,
-            node_dim=None):
-        self.M = n_heads
-        self.d_h = embed_dim
-        self.N = n_layers
-        self.n_nodes = len(x)
-        # self.time_step = 0
-        self.W_q = nn.Parameter(torch.random(self.d_h, self.d_h))
-        self.W_k = nn.Parameter(torch.random(self.d_h, self.d_x))
-        self.W_v = nn.Parameter(torch.random(self.d_h, self.d_x))
-        self.context_node = [embedded_graph[1], torch.dot(self.W_v, embedded_graph[0][initial_node]),
-                             torch.dot(self.W_v, embedded_graph[0][final_node])]
+            key_dim,
+            tanh_clipping=10):
 
-    def forward(self):
-        not_visited = np.ones(self.n_nodes)
-        for time_step in range(self.n_nodes):
-            self.context_node = new_attention_mechanism(self.context_node)
+        # Primitive attributes
+        self.n_heads = n_heads
+        self.input_dim = embed_dim  # dimension of h
 
+        # Mechanism-dependent attributes
+        self.key_dim = key_dim
+        self.tanh_clipping = tanh_clipping
+
+        self.compat_factor = 1 / math.sqrt(key_dim)  # faster than **(-0.5)
+
+        self.W_Q_graph = nn.Parameter(torch.randn((n_heads, embed_dim, key_dim)))
+        self.W_Q_nodes = nn.Parameter(torch.randn((n_heads, 2 * embed_dim, key_dim)))
+        self.W_K = nn.Parameter(torch.randn((n_heads, embed_dim, key_dim)))
+
+
+    def initialize_params(self, init_function):
+        """Initialization."""
+        init_function(self.W_Q)
+        init_function(self.W_K)
+
+
+    def forward(self, h, h_graph):
+
+
+        batch_size, graph_size, input_dim = h.size()
+        h = h.view(batch_size, 1, graph_size, input_dim)
+
+        # q_graph : (batch_size, n_heads, graph_size, key_dim)
+        q_graph = torch.matmul(h_graph, self.W_Q_graph)  # Can be calculated outside the loop because constant graph embedding
+
+        log_probs = []
+        sequences = []
+        # 0 --> 1 & 1 --> -inf
+        visited = np.zeros(graph_size)
+
+        for time_step in range(graph_size):
+
+            # Computing compatibilities
+
+            h_nodes = self._get_context_nodes(self, h, sequences)
+            # q_nodes : (batch_size, n_heads, graph_size, key_dim)
+            q_nodes = torch.matmul(h_nodes, self.W_Q_nodes)
+            # k : (batch_size, n_heads, graph_size, key_dim
+            k = torch.matmul(h, self.W_K)
+
+            # compatibility : (batch_size, n_heads, graph_size, graph_size)
+            compatibility = self.compat_factor * torch.matmul(q, k.transpose(2, 3))
+
+            # From the logits compute the probabilities by clipping
+            if self.tanh_clipping > 0:
+                compatibility = F.tanh(compatibility) * self.tanh_clipping
+
+            mask_temp = np.clip((0.5 - visited)*np.inf, -np.inf, 1)  # TOCHECK -- there may be a better implementation since we make the mask ourselves
+            mask_temp = torch.from_numpy(mask_temp)
+            compatibility = compatibility * mask_temp
+
+            # attention : (batch_size, n_heads, graph_size, graph_size)
+            attention = F.softmax(compatibility, dim=-1)  # TODO maybe one day we can add a temperature
+
+            idx_x, idx_y = np.where(visited != 1)  # TOCHECK - not needed normally
+            attention[:, :, idx_x, idx_y] = 0
+
+            # Select the indices of the next nodes in the sequences (or evaluate eval_seq), result (batch_size) long
+            selected = self._select_node(log_probs, visited) if eval_seq is None else eval_seq[:, i]
+
+            log_probs.append(attention.log())
+            sequences.append(selected)
+
+            #  Updating mask
+            visited = visited.clone().scatter_(1, selected.unsqueeze(-1), True)
+
+        # Collected lists, return Tensor
+        return torch.stack(outputs, 1), torch.stack(sequences, 1)
+
+
+
+    def _select_node(self, log_probs, mask):
+
+        if self.decode_type == "greedy":
+            _, selected = log_probs.max(1)
+            assert not mask.gather(1, selected.unsqueeze(
+                -1)).data.any(), "Decode greedy: infeasible action has maximum probability"
+
+        # elif self.decode_type == "sampling":  # TODO for later
+        #     selected = probs.multinomial(1).squeeze(1)
+        #
+        #     # Check if sampling went OK, can go wrong due to bug on GPU
+        #     # See https://discuss.pytorch.org/t/bad-behavior-of-multinomial-function/10232
+        #     while mask.gather(1, selected.unsqueeze(-1)).data.any():
+        #         print('Sampled bad values, resampling!')
+        #         selected = probs.multinomial(1).squeeze(1)
+
+        else:
+            assert False, "Unknown decode type"
+        return selected
+
+
+    def _get_context_nodes(self, embeddings, sequences):
+        """
+        Finds the context. If it is the first step (no sequences), we have it from placeholders. Else, we keep the first sequence, the last one.
+        """
+        # self.context_node = [embedded_graph[1], torch.dot(self.W_v, embedded_graph[0][initial_node]),
+        #                      torch.dot(self.W_v, embedded_graph[0][final_node])]
+        if len(sequences) == 0:
+            # First step, use learned input symbol (placeholder)
+            # No need to repeat, by adding dimension broadcasting will work
+            return self.W_placeholder.unsqueeze(0)
+        else:
+            batch_size = embeddings.size(0)
+            # Return first and last node embeddings
+            return torch.gather(
+                embeddings,
+                1,
+                torch.stack((sequences[0], sequences[-1]), dim=1)
+                .contiguous()
+                .view(batch_size, 2, 1)
+                .expand(batch_size, 2, embeddings.size(-1))
+            ).view(batch_size, -1)  # View to have (batch_size, 2 * embed_dim)
